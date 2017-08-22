5ccf495cce161c00195ecfcf9994da540bb992bc
gdmec07150942 <554061700@qq.com>
2017-08-22 16:15:27 +0800

    add ifDemo

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,23 @@
     <div v-bind:class="{'class1': class1}">来来来，变颜色了</div>
     <br>
     <p v-if="seen">现在你看到我了</p>
-    <h1><a v-bind:href="url">涛哥Vue练习的demo的github</a></h1>
+    <h1><a :href="url">涛哥Vue练习的demo的github</a></h1>
     <p>{{msg}}</p>
-    <button v-on:click="fanzhuangMessage">翻转字符串</button>
+    <button @click="fanzhuangMessage">翻转字符串</button>
     <input v-model="msg">
     <template v-if="ok">
       <h1>好好好</h1>
       <p>您好</p>
     </template>
+    <h1 v-if="Math.random() >0.5">
+      哈哈哈
+    </h1>
+    <h1 v-else>
+      嘻嘻
+    </h1>
+    <h1 v-if="type === 'A'">A</h1>
+    <h1 v-else-if="type === 'B'">B</h1>
+    <h1 v-else-if="type === 'C'">C</h1>
   </div>
 </template>
 
@@ -34,7 +43,8 @@
         seen: false,
         ok: false,
         url: 'https://github.com/gdmec07150942/VueDemo',
-        msg: '嘻嘻'
+        msg: '嘻嘻',
+        type: 'A'
       }
     },
     methods: {
